713a6d4086419e6fb016fde690690cddcfabd394
Joseph Prospero <jprospero413@gmail.com>
2021-01-21 17:38:15 -0800

    linked the two html pages

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,29 +1,41 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
-var getRepoIssues = function(repo) {
-    console.log(repo);
-    
+var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-    fetch(apiUrl);
 
-    fetch(apiUrl).then(function(response) {
+    fetch(apiUrl).then(function (response) {
         if (response.ok) {
-            response.json().then(function(data) {
+            response.json().then(function (data) {
                 displayIssues(data);
+
                 if (response.headers.get("Link")) {
                     displayWarning(repo);
                 }
             });
         }
         else {
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html")
         }
     });
 
 };
 
-var displayIssues = function(issues) {
+var getRepoName = function () {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1]; 
+
+    if(repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+
+    } else {
+        document.location.replace("./index.html");
+    }
+};
+
+var displayIssues = function (issues) {
     if (issues.length === 0) {
         issueContainerEl.textContent = "This repo has no open issues!";
         return;
@@ -53,9 +65,9 @@ var displayIssues = function(issues) {
     };
 };
 
-var displayWarning = function(repo) {
+var displayWarning = function (repo) {
     limitWarningEl.textContent = "To see more than 30 issues, visit ";
- 
+
     var linkEl = document.createElement("a");
     linkEl.textContent = "See More Issues on GitHub.com";
     linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
@@ -64,4 +76,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
