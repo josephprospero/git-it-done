34ff70e178e263343fd4a8a558bf0cbdd3ecdf47
Joseph Prospero <jprospero413@gmail.com>
2021-01-22 20:06:25 -0800

    added filtering of issues

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,48 +2,73 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
-var formSubmitHandler = function(event) {
+var formSubmitHandler = function (event) {
     event.preventDefault();
 
     var username = nameInputEl.value.trim();
 
     if (username) {
         getUserRepos(username);
+        
+        repoContainerEl.textContent = "";
         nameInputEl.value = "";
     } else {
         alert("Please enter a GitHub username")
     }
-
-    console.log(event);
 };
 
-var getUserRepos = function(user) {
-    apiUrl = "https://api.github.com/users/" + user + "/repos";
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    
+    if (language) {
+        getFeaturedRepos(language);
+
+        repoContainerEl.textContent = "";
+    };
+};
+
+var getUserRepos = function (user) {
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
+                displayRepos(data, user);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    })
+        .catch(function (error) {
+            alert("Unable to connect to GitHub");
+        });
+};
+
+var getFeaturedRepos = function (language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
 
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-            displayRepos(data, user);
-        });
+                displayRepos(data.items, language);
+            });
         } else {
-            alert ("Error: " + response.statusText);
+            alert("Error: " + response.statusText);
         }
-    })
-
-    .catch(function(error) {
-        alert ("Unable to connect to GitHub");
     });
 };
 
-var displayRepos = function(repos, searchTerm) {
+var displayRepos = function (repos, searchTerm) {
     if (repos.length === 0) {
         repoContainerEl.textContent = "No repositories found.";
         return;
     }
-    repoContainerEl.textContent = "";
+    
     repoSearchTerm.textContent = searchTerm;
-   
+
     for (var i = 0; i < repos.length; i++) {
         var repoName = repos[i].owner.login + "/" + repos[i].name;
 
@@ -55,20 +80,22 @@ var displayRepos = function(repos, searchTerm) {
         titleEl.textContent = repoName;
 
         repoEl.appendChild(titleEl);
-    var statusEl = document.createElement("span");
-    statusEl.classList = "flex-row align-center";
 
-    if (repos[i].open_issues_count > 0) {
-        statusEl.innerHTML = 
-            "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + "issue(s)";
-    
-    } else {
-        statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML =
+                "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + "issue(s)";
+
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
         }
 
         repoEl.appendChild(statusEl);
-    repoContainerEl.appendChild(repoEl);
+        repoContainerEl.appendChild(repoEl);
     }
 };
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
